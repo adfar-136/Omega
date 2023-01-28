17cf9db7eb8b5ad66526f772bebb9345aa0dbc41
adfar-136 <adfarrasheed136@gmail.com>
2023-01-28 12:54:29 +0530

    hgff

---
+++ JavaScript/date.js
@@ -0,0 +1,75 @@
+// var a = new Date()
+// console.log(a)
+
+// var a = new Date(234236567674)
+// console.log(a)
+
+// var a = new Date('Jan 22, 2023 09:20:35')
+// console.log(a)
+
+// var b = new Date(1998,10,13,5,30,22,1300).toLocaleString()
+// console.log(b)
+
+// var a = new Date().toLocaleString()
+// console.log(a)
+
+// var date =new Date()
+// var day = date.getDate()
+// console.log(day)
+// var month = date.getMonth()+1
+// console.log(month)
+// var year = date.getFullYear()
+// console.log(year)
+// console.log("Current date is :", day,"/",month,"/",year)
+
+// var today =new Date()
+
+// var h = today.getHours()
+// var m = today.getMinutes()
+// var s = today.getSeconds()
+
+// console.log(h,":",m,":",s)
+
+// let vall =  new Date("2020-08-01")
+// // let val =  new Date("2020-08")
+// // let val =  new Date("2020")
+
+// console.log(vall)
+
+// let val = new Date("08/01/2020")
+// console.log(val)
+
+// let val = new Date("Aug 01 2020").toLocaleString()
+// console.log(val)
+
+// let a = new Date()
+// a.setFullYear(2020)
+// console.log(a.toLocaleString())
+
+// var d1 = new Date("08/13/2022")
+// var d2 = new Date("08/14/2022")
+// var diff = d2.getTime()- d1.getTime()
+// const daydiff = diff/(1000*60*60*24)
+// console.log(daydiff)
+
+// var d1 = new Date("10/02/1965")
+// var d2 = new Date()
+// var diff = d2.getTime()- d1.getTime()
+// console.log(diff)
+// const yeardiff = (diff/31536000000).toFixed(0)
+// console.log(yeardiff)
+
+
+var val = new Date("2020")
+ console.log(val)
+
+ var val = new Date("2020-08-01")
+ console.log(val)
+
+var val = new Date("Aug 01 2020").toLocaleString()
+console.log(val)
+
+
+
+
+
---
+++ JavaScript/lexical.js
@@ -0,0 +1,10 @@
+function a(){
+    
+    var b = 40
+    c()
+   function c(){
+    console.log(b)
+   }
+}
+
+a()
